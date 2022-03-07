6583f5ff4b700b9b7eb93de5b05ce6f8759ff98d
German Morone <GMORONE@prismamp.com>
2022-03-07 20:31:50 -0300

    inicio solution5

---
+++ restaurant/js/script.js
@@ -0,0 +1,48 @@
+
+//Collapsable bottom
+$(function(){
+    $("#navbarToggle").blur(function(event){
+        var screenWidth = window.innerWidth;
+        console.log(screenWidth);
+        if (screenWidth < 768){
+            $("#collapsable-nav").collapse('hide');
+        }
+    });
+});
+
+(function(global){
+    var dc ={};
+ //   var homeHtml = "<C:\Users\gmorone\Coursera-test\restaurant\snippets\home-snippet.html>";
+    var homeHtml = "snippets/home-snippet.html";
+    var insertHtml = function (selector, html){
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    };
+    var showLoading = function(selector){
+        var html = "<div class='text-center'>";
+        html += "<img src='images/loading-bar.gif'></div>";
+        insertHtml(selector,html);
+    };
+    
+    document.addEventListener("DOMContentLoaded", function(event){
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(homeHtml, function (responseText){
+                document.querySelector("#main-content").innerHTML = responseText;
+            }, false);        
+    });
+    global.$dc = dc;
+})(window);
+
+
+$.ajax({
+    url: "homeHtml.html",
+    sendGetRequest : function(responseText){
+        $('#main-content').text('responseText');
+    },
+    success : function(data){
+        setTimeout(function(){
+            $('#contenido').html(data);
+        },esperar
+        );
+    }
+});
